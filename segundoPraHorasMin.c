bf91e5ffa7d3815c93d2b472cbd3a6299380ff07
marcelodalves <43051943+marcelodalves@users.noreply.github.com>
2020-03-10 20:50:02 -0300

    Create segundoPraHorasMin.c

---
+++ segundoPraHorasMin.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+  int num_seg, segundos, minutos, hora, resto;
+  
+  printf("Entre com o numero de segundos: ");
+  scanf("%d", &num_seg); 	
+  
+  hora = num_seg/3600;
+  resto = num_seg%3600;
+  minutos = resto/60;
+  segundos= resto%60;
+  
+  printf("\n%dh %dhmin %ds",hora,minutos,segundos);
+  
+  return 0;
+}
